96469a47283463a2944947028bceb745fc26d605
Carmine D'Amico <carmine.damico@bike.center>
2020-03-16 19:30:11 +0100

    add spinner when polling isn't started

--- src/client/components/TvAliveBadge.tsx
+++ src/client/components/TvAliveBadge.tsx
@@ -1,15 +1,17 @@
 import * as React from "react";
-import { Badge } from "react-bootstrap";
+import { Badge, Spinner } from "react-bootstrap";
 import fetch from "cross-fetch";
 import Poller from "../lib/poller";
 
 const TvAliveBadge = () => {
+  const [isPolling, setIsPolling] = React.useState(false);
   const [isAlive, setIsAlive] = React.useState(false);
 
   const getIsTvAlive = () => {
     return fetch("http://localhost:4321/tv").then((jsonResponse: Response) => {
       jsonResponse.json().then((tvInfo: any) => {
         setIsAlive(tvInfo.isAlive);
+        setIsPolling(true);
       });
     });
   };
@@ -18,7 +20,11 @@ const TvAliveBadge = () => {
   poller.onPoll(getIsTvAlive);
   poller.poll();
 
-  return isAlive ? (
+  return !isPolling ? (
+    <Spinner animation="border" role="status">
+      <span className="sr-only">Loading...</span>
+    </Spinner>
+  ) : isAlive ? (
     <Badge variant="success">On</Badge>
   ) : (
     <Badge variant="danger">Off</Badge>
